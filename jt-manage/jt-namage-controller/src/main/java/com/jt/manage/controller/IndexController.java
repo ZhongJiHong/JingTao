ee2636d009635453a3b35d12311701ab2988fa25
zhong <zhong_jh4848@163.com>
2017-12-15 21:55:51 +0800

    frame finished

---
+++ jt-manage/jt-namage-controller/src/main/java/com/jt/manage/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.jt.manage.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping("/page/{index}")
+	public String index(@PathVariable String index) {
+
+		return index;
+	}
+}
